5f8100cb298b5f5e2fce18c97491f7a82f3f3458
mateasmario <mmmateas@gmail.com>
2023-10-22 21:05:24 +0300

    Added main method bodies to MessageService and MessageController

--- backend/src/main/java/com/timetravellers/backend/controllers/MessageController.java
+++ backend/src/main/java/com/timetravellers/backend/controllers/MessageController.java
@@ -1,7 +1,29 @@
 package com.timetravellers.backend.controllers;
 
+import com.timetravellers.backend.services.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
+    @Autowired
+    private MessageService messageService;
+
+    @GetMapping("/messages/id/{id}")
+    public ResponseEntity getById(@PathVariable String id) {
+        return null;
+    }
+
+    @GetMapping("/messages/recipient/{recipient}")
+    public ResponseEntity getByRecipient(@PathVariable String recipient) {
+        return null;
+    }
+
+    @GetMapping("/messages/isPublic/{isPublic}")
+    public ResponseEntity getByIsPublic(@PathVariable boolean isPublic) {
+        return null;
+    }
 }
--- backend/src/main/java/com/timetravellers/backend/services/MessageService.java
+++ backend/src/main/java/com/timetravellers/backend/services/MessageService.java
@@ -1,11 +1,27 @@
 package com.timetravellers.backend.services;
 
+import com.timetravellers.backend.entities.Message;
 import com.timetravellers.backend.repositories.MessageRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MessageService {
     @Autowired
     private MessageRepository messageRepository;
+
+    public Message findById(ObjectId id) {
+        return null;
+    }
+
+    public List<Message> findByRecipient(String recipient) {
+        return null;
+    }
+
+    public List<Message> findByIsPublic(boolean isPublic) {
+        return null;
+    }
 }
