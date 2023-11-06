ada9241b691e4052052906df76c87ff9cef15480
giuliamandres <93037838+giuliamandres@users.noreply.github.com>
2023-11-07 00:37:40 +0200

    created the changed password method with encoding integrated.

--- backend/src/main/java/com/timetravellers/backend/security/config/SecurityConfiguration.java
+++ backend/src/main/java/com/timetravellers/backend/security/config/SecurityConfiguration.java
@@ -9,6 +9,8 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -32,5 +34,10 @@ public class SecurityConfiguration {
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
     }
 }
--- backend/src/main/java/com/timetravellers/backend/services/UserService.java
+++ backend/src/main/java/com/timetravellers/backend/services/UserService.java
@@ -4,6 +4,7 @@ import com.timetravellers.backend.entities.mongodb.User;
 import com.timetravellers.backend.repositories.UserRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +12,28 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User findById(ObjectId id){
         return null;
     }
 
-    public boolean changePassword(String userId, String oldPassword, String newPassword){
-        return false;
+    public boolean changePassword(ObjectId userId, String oldPassword, String newPassword){
+        User user = userRepository.findById(userId).orElse(null);
+        if (user == null) {
+            return false; // User not found
+        }
+
+        // Verify the old password
+        if (!passwordEncoder.matches(oldPassword, user.getPassword())) {
+            return false; // Old password is incorrect
+        }
+
+        // Encode and update the new password
+        user.setPassword(passwordEncoder.encode(newPassword));
+        userRepository.save(user);
+
+        return true; // Password changed successfully
     }
 }
