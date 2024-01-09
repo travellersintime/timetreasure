66a1c41a275fb61bf3d87d7feef23f8c3eaa7280
giuliamandres <93037838+giuliamandres@users.noreply.github.com>
2024-01-09 21:19:35 +0200

    Updated footer feature to change color of the items accordingly

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -4,21 +4,32 @@ import { faHouse } from '@fortawesome/free-solid-svg-icons/faHouse';
 import { faCirclePlus } from '@fortawesome/free-solid-svg-icons/faCirclePlus';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { useNavigation } from '@react-navigation/native';
+import React, { useState, useEffect } from 'react';
 
 
 const Footer = (props: Props) => {
     const navigation = useNavigation();
+    const [activeIcon, setActiveIcon] = useState('MessageFeed');
+
+    useEffect(() => {
+    // Navigate to the activeIcon route after state update
+    navigation.navigate(activeIcon);
+  }, [activeIcon, navigation]);
+
+    const handleIconClick = (routeName) => {
+    setActiveIcon(routeName); // Update active icon immediately
+  };
 
     return (
         <View style={{flex: 0.1, flexDirection: 'row', justifyContent: 'space-around', marginTop: 10}}>
-            <TouchableOpacity style={styles.bottomBtn} onPress={() => navigation.navigate('MyProfile')}>
-                <FontAwesomeIcon icon={faUser} size={30}/>
+            <TouchableOpacity style={styles.bottomBtn} onPress={() => handleIconClick('MyProfile')}>
+                <FontAwesomeIcon icon={faUser} size={30} color={activeIcon === 'MyProfile' ? '#fb5b5a' : null}/>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.bottomBtn} onPress={() => navigation.navigate('MessageFeed')}>
-                <FontAwesomeIcon icon={faHouse} size={30}/>
+            <TouchableOpacity style={styles.bottomBtn} onPress={() => handleIconClick('MessageFeed')}>
+                <FontAwesomeIcon icon={faHouse} size={30} color={activeIcon === 'MessageFeed' ? '#fb5b5a' : null}/>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.bottomBtn} onPress={() => navigation.navigate('CreateTextMessage')}>
-                <FontAwesomeIcon icon={faCirclePlus} size={30}/>
+            <TouchableOpacity style={styles.bottomBtn} onPress={() => handleIconClick('CreateTextMessage')}>
+                <FontAwesomeIcon icon={faCirclePlus} size={30} color={activeIcon === 'CreateTextMessage' ? '#fb5b5a' : null}/>
             </TouchableOpacity>
         </View>
     );
